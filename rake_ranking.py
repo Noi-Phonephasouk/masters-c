ffab7f510f7554c0d1a89ae4ce30ad700ef998d0
Noi-Phonephasouk <khamnoi111@gmail.com>
2017-07-07 13:03:55 +1200

    Add files via upload

--- rake_ranking.py
+++ rake_ranking.py
@@ -4,58 +4,59 @@ import re
 import operator
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
-from pandas import DataFrame
+import pandas as pd
 import sqlite3
 
 def replace_abbr_word(wrd_lst):
-   
-    abbr_file = open("abbreviations_list.csv", "r")    
-    handler = csv.reader(abbr_file)
-    abbr_words = []
-    full_words = []
+	'''To replace a list of abbreviated words to full words'''
+	abbr_file = open("abbreviations_list.csv", "r")    
+	handler = csv.reader(abbr_file)
+	abbr_words = []
+	full_words = []
     
-    for line in handler:
-        if line[0] not in abbr_words:
-            abbr_words.append(line[0]) 
-        if line[1] not in full_words:
-            full_words.append(line[1]) 
+	for line in handler:
+		if line[0] not in abbr_words:
+			abbr_words.append(line[0]) 
+		if line[1] not in full_words:
+			full_words.append(line[1]) 
             
-    abbr_file.close() 
-    for i, j in enumerate(wrd_lst):
-        for k,l in enumerate(abbr_words):
-            if j.lower() == l:                
-                wrd_lst[i] = full_words[k]         
-    return wrd_lst
+	abbr_file.close() 
+	for i, j in enumerate(wrd_lst):
+		for k,l in enumerate(abbr_words):
+			if j.lower() == l:                
+				wrd_lst[i]=full_words[k]         
+	return wrd_lst
 	
 def contractions_replace(text):
-	
-	text = text.lower()
+	'''To replace a list of contracted words to full words'''
+	text=text.lower()
 	cont_file = open("contractions_list.csv", "r")    
 	handler = csv.reader(cont_file)
 	contraction_lst = []
 		
 	for line in handler:
-		contraction_lst.append({'cont_wrd':line[0], 'full_wrd':line[1]})         
-	cont_file.close()  
-	
-	cont_dataframe = DataFrame(contraction_lst)
+		contraction_lst.append({'cont_wrd':line[0], 'full_wrd':line[1]})       
+		
+	cont_file.close()  	
+	cont_dataframe = pd.DataFrame(contraction_lst)
 	
 	for i in range(0, len(cont_dataframe)):
 		if cont_dataframe.iloc[i]['cont_wrd'] in text:
-			text = text.replace(cont_dataframe.iloc[i]['cont_wrd'], cont_dataframe.iloc[i]['full_wrd'])
-       
+			text = text.replace(cont_dataframe.iloc[i]['cont_wrd'], cont_dataframe.iloc[i]['full_wrd'])   
 	return text
 	
-def remove_puncz(sentence):	 
-	tokenizer = RegexpTokenizer(r"\w+")
+def remove_puncz(sentence):
+	'''To remove punctuations from a sentnece'''
+	tokenizer= RegexpTokenizer(r"\w+")
 	tokens = tokenizer.tokenize(sentence)
 	return " ".join(tokens)
 	
 def feature_cull(txt_sent):
+	'''To select key features by pre-processing techniques'''
 	lemmatizer = nltk.WordNetLemmatizer()
 	stemmer = nltk.stem.porter.PorterStemmer()
 	txt_sent = contractions_replace(txt_sent)
-	txt_sent = re.sub(r"^\d+\W\s" , "", txt_sent)
+	txt_sent= re.sub(r"^\d+\W\s" , "", txt_sent)
 	txt_sent = remove_puncz(txt_sent)
 	txt_sent = txt_sent.lower()	
 	new_sent_ls = word_tokenize(txt_sent)
@@ -120,16 +121,25 @@ def build_stop_word_regex(stop_word_file_path):
 	return stop_word_pattern
 
 def generate_candidate_keywords(sentence_list, stopword_pattern):
-    phrase_list = []
-    for s in sentence_list:
-        tmp = re.sub(stopword_pattern, '|', s.strip())
-        phrases = tmp.split("|")
-        for phrase in phrases:
-            phrase = phrase.strip().lower()
-            if phrase != "":
-                phrase_list.append(phrase)
-    return phrase_list
-
+	phrase_list = []
+	phrase_sent = []
+	itm = ''
+	for s in sentence_list:
+		tmp = re.sub(stopword_pattern, '|', s.strip())
+		#print tmp, len(tmp)
+		phrases = tmp.split("|")
+		for phrase in phrases:
+			phrase = phrase.strip().lower()
+			if phrase != "" and phrase not in phrase_list:
+				phrase_list.append(phrase)
+		#print phrase_list
+		for val in phrase_list:
+			itm += val + " "
+		
+		phrase_sent.append(itm)
+		itm = ''
+		phrase_list = []
+	return phrase_sent
 
 def calculate_word_scores(phraseList):
     word_frequency = {}
@@ -153,7 +163,6 @@ def calculate_word_scores(phraseList):
         word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)
     return word_score
 
-
 def generate_candidate_keyword_scores(phrase_list, word_score):
     keyword_candidates = {}
     for phrase in phrase_list:
@@ -168,7 +177,7 @@ def generate_candidate_keyword_scores(phrase_list, word_score):
 conn = sqlite3.connect('ureviewdata.sqlite')
 curs = conn.cursor()
 
-cur = curs.execute('''SELECT date, review, sent_group, sent_group.sentid,truecatid, truebiclassid, category, username,appname FROM sent_categorize,category,comments,sent_breakdown, sent_group, users, appname,webs WHERE sent_breakdown.cid = comments.cid AND sent_group.sentid = sent_breakdown.sentid AND sent_group.sentid=sent_categorize.sentid AND catid = truecatid AND users.uid = comments.uid AND users.wid = webs.wid AND webs.appid = appname.appid AND date BETWEEN "2010-01-01" AND "2016-12-31" AND appname = "Jira" ''')
+cur = curs.execute('''SELECT date, review, sent_group, sent_group.sentid,truecatid, truebiclassid, category, username,appname FROM sent_categorize,category,comments,sent_breakdown, sent_group, users, appname,webs WHERE sent_breakdown.cid = comments.cid AND sent_group.sentid=sent_breakdown.sentid AND sent_group.sentid=sent_categorize.sentid AND catid=truecatid AND users.uid=comments.uid AND users.wid=webs.wid AND webs.appid=appname.appid AND date BETWEEN "2010-01-01" AND "2016-12-31" AND appname ="Jira" ''')
 
 #BETWEEN "2015-01-01" AND "2016-12-31" (YYY-MM-DD)- This allows using different date range to filter data
 
@@ -181,34 +190,52 @@ sim_data_cat = []
 sim_sentls_set = []
 lst_sent_classid = []
 
-for row in rows:
-	if row[4] not in sim_data_cat:
-		sim_data_cat.append(int(row[4]))
-		
-	sim_sentls_set.append({'sentid':int(row[3]), 'review_sent':row[2], 'classid':int(row[4])})
-conn.commit()
+sim_sent_chk = []
+sim_sentls_class =[]
+lst_sent_idx = []
 
+for row in rows:
+	if not any(d.get('clsid', None) == int(row[4]) for d in sim_data_cat):
+		sim_data_cat.append({"clsid": int(row[4]), "clsname":row[6]})
+	if row[2] not in sim_sent_chk:
+		sim_sent_chk.append(row[2])
+		sim_sentls_set.append({'sentid':int(row[3]), 'review_sent':row[2], 'classid':int(row[4])})
+		
+conn.commit()
 sim_data_cat.sort()
 
 for j in range(0,len(sim_data_cat)):
 	for i in range(0, len(sim_sentls_set)):
-		if sim_data_cat[j] == sim_sentls_set[i]['classid']:
-			lst_sent_classid.append(sim_sentls_set[i]['review_sent'])
-			sim_data.append(sim_sentls_set[i]['review_sent'])
+		if sim_data_cat[j]['clsid'] ==sim_sentls_set[i]['classid']:
+			lst_sent_classid.append({'sent':str(sim_sentls_set[i]['review_sent']), 'sentid': int(sim_sentls_set[i]['sentid']), 'class':sim_sentls_set[i]['classid']})
+			lst_sent_idx.append(int(sim_sentls_set[i]['sentid']))
+			
+	df = pd.DataFrame(lst_sent_classid, index = lst_sent_idx)	
 	
-	phraseList = generate_candidate_keywords(lst_sent_classid,stoppath)
+	phraseList = generate_candidate_keywords(df.sent,stoppath)
 	word_score = calculate_word_scores(phraseList)
 	keywordcandidates = generate_candidate_keyword_scores(phraseList,word_score)
-	sortedKeywords = sorted(keywordcandidates.iteritems(), key = operator.itemgetter(1), reverse=True)
-	print "Category ", sim_data_cat[j]
+	sortedKeywords = sorted(keywordcandidates.iteritems(), key=operator.itemgetter(1), reverse=True)
+	
+	pl = pd.Series(phraseList, index=df.index)	
+	df = df.assign(pl = pl.values)
+	
+	print "CATEGORY", sim_data_cat[j]['clsid'], sim_data_cat[j]['clsname'], "\n"
 	
 	for keyword in sortedKeywords[0:5]:
-		print "Keyword" , str(sortedKeywords.index(keyword)+1)+ ": " + keyword[0], ", score: ", keyword[1]
-
+		for i in range(0, len(df)):		
+			if keyword[0] == df.iloc[i]['pl']:
+				print "Sentence" , str(sortedKeywords.index(keyword)+1)+ ": " + df.iloc[i]['sent'], "Keyword scores: %.3f" % keyword[1] #  + keyword[0]
+	
+	df.drop(df.index, inplace=True)
+	pl.drop(pl.index, inplace=True)
+	
 	lst_sent_classid=[]
-	print
+	lst_sent_idx=[]
+	print "\n"
+	
 
-phraseList = generate_candidate_keywords(sim_data,stoppath)
+phraseList = generate_candidate_keywords(sim_sent_chk,stoppath)
 word_score = calculate_word_scores(phraseList)
 keywordcandidates = generate_candidate_keyword_scores(phraseList,word_score)
 
